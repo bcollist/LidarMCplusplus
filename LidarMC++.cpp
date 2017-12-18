191b935836cb5843b1fca3c835b133ef5ba5d6ff
bcollist <bcoll018@odu.edu>
2017-12-18 17:19:02 -0500

    Fixed more compiling errors from transfer from python. Added armadillo matrix definitions to the 'intersectionAngle' subroutine.

--- LidarMC++.cpp
+++ LidarMC++.cpp
@@ -215,12 +215,25 @@ double intersectionAngle(double x1,double y1,double z1,double x2,double y2,doubl
 // intersectionAngle(c1,c2) - Calculates the intersection angle between a photon trajectory and the plane made by the lidar detector
 // In order to determine if a photon has entered the detector within the FOV of the detector, this function calculates the
 // angle between the unit vector normal to the detector plane and the propegation vector
-  colvec c1; colvec c2;
-  c1 = vec(x1,y1,z1);   // an array containing the first x,y,z points of the propegation vector
-  c2 = vec(x2,y2,z2);   // an array containing the second x,y,z points of the propegation vector
-  u = vec(0,0,1);   // create a unit vector normal to the plane of the detector at the origin
+  colvec c1; colvec c2; colvec u; colvec v;
+  double angle;
+
+  c1   << x1 << endr  // an array containing the first x,y,z points of the propegation vector
+       << y1 << endr  // vector declarations are made using c++94 style
+       << z1 << endr;
+
+  c2   << x2 << endr // an array containing the first x,y,z points of the propegation vector
+       << y2 << endr  // vector declarations are made using c++94 style
+       << z2 << endr; // an array containing the second x,y,z points of the propegation vector
+
+  u   << 0 << endr
+      << 0 << endr
+      << 1 << endr;   // create a unit vector normal to the plane of the detector at the origin
+
   v = c2 - c1;   // convert the propegation vector to a unit vector
+
   angle = atan2(norm(cross(u,v)),dot(u,v)); // calculate the angle between the propegation and the vector normal to the detector plane
+
   return angle;
 }
 
