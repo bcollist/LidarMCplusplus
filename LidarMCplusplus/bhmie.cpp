05d1c58e8d3994f4f2f5de7abf4f52d10fab10c4
bcollist <bcoll018@odu.edu>
2018-01-18 17:48:11 -0500

    I translated a version of mie code to C++ for inclusion in the model. At first glance, it validates against a mie code converted to matlab by Emmanuel Boss, but I should do a more rigorous validation tomorrow when Im not tired and can think straight. Next step is to integrate this into the model.

--- LidarMCplusplus/bhmie.cpp
+++ LidarMCplusplus/bhmie.cpp
@@ -4,124 +4,160 @@
 #include <cmath>
 #include <iostream>
 #include <complex>
-#include <vector>
 
 using namespace std;
 
-//function [S1,S2,Qscat,Qext,Qback]=bhmie(x,refrel,nang);
-
-// Variable Prototypes
-
+// Global Variable Prototypes
 double pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062;
+complex <double> imaginary = sqrt(complex<double>(-1,0));
+// Function Prototypes
+int bhmie(double x, double refrel, int nang);
 
+
+// Main Function
 int main(){
-    // Variable Prototypes
+    
+    // Variables //
     
     // User Inputs
-    double x; // size parameter
-    double refrel; // relative refractive index
-    int nang; // number of angles between
+    double x = 2; // size parameter
+    double refrel = 1.33;//(1.3, 0.008); // relative refractive index
+    int nang = 91; // number of angles between
     
-    // Calculation Variables
-    double y;
-    double dx;
-    double nstop; // series terminated after nstop terms
-    double ymod;
-    int nmx;
-    int nn;
-    double dang;
-    double theta[nang];
-    double AMU[nang];
-    int RN;
-    vector<complex <double> > D (nmx,0);
-    
-    dx = x;
-    y = x * refrel;
-    
-    nstop = ceil(x + 4 * pow(x,0.3333) +2);
-    ymod = abs(y);
-    nmx = max(nstop,ceil(ymod))+15;
-    dang = pi/2/(nang-1);
-    
-    for (int i; i<nang; i++){
-        theta[i] = i * dang;
-        AMU[i] =  cos(theta[i]);
-    }
-    //Logarithmic derivative D(j) calculated by downward recurence
-    nn = nmx-1;
-    for (int n; n<nn; n++){
-        RN = nmx-n+1;
-        D(nmx-n)=(RN/y)-(1./(D(nmx-n+1)+RN/y));
-    }
-    
-        
-    }
-    
-
+    bhmie(x, refrel, nang);
+    return 0;
 }
 
 
-//for n=1:nn
-//RN=nmx-n+1;
-//D(nmx-n)=(RN/y)-(1./(D(nmx-n+1)+RN/y));
-//end
-//PI0=zeros(nang,1);
-//PI1=ones(nang,1);
-//nn=2*nang-1;
-//S1=zeros(nn,1);
-//S2=zeros(nn,1);
-//%Riccati-Bessel functins with real argument x calculated by upward recurence
-//PSI0=cos(dx);
-//PSI1=sin(dx);
-//CHI0=-sin(x);
-//CHI1=cos(x);
-//APSI0=PSI0;
-//APSI1=PSI1;
-//XI0=APSI0-CHI0*sqrt(-1);
-//XI1=APSI1-CHI1*sqrt(-1);
-//Qscat=0.0;
-//n=1;
-//while(n-1-nstop<0)
-//DN=n;
-//RN=n;
-//FN=(2*RN+1)/(RN*(RN+1));
-//PSI=(2*DN-1)*PSI1/dx-PSI0;
-//APSI=PSI;
-//CHI=(2*RN-1)*CHI1/x-CHI0;
-//XI=APSI-CHI*sqrt(-1);
-//AN=((D(n)/refrel+RN/x)*APSI-APSI1)/((D(n)/refrel+RN/x)*XI-XI1);
-//BN=((refrel*D(n)+RN/x)*APSI-APSI1)/((D(n)*refrel+RN/x)*XI-XI1);
-//Qscat=Qscat+(2.*RN+1)*(abs(AN)^2+abs(BN)^2);
-//for j=1:nang
-//jj=2*nang-j;
-//PI(j)=PI1(j);
-//TAU(j)=RN*AMU(j)*PI(j)-(RN+1)*PI0(j);
-//P=(-1)^(n-1);
-//S1(j)=S1(j)+FN*(AN*PI(j)+BN*TAU(j));
-//T=(-1)^n;
-//S2(j)=S2(j)+FN*(AN*TAU(j)+BN*PI(j));
-//if(j~=jj)
-//S1(jj)=S1(jj)+FN*(AN*PI(j)*P+BN*TAU(j)*T);
-//S2(jj)=S2(jj)+FN*(BN*PI(j)*P+AN*TAU(j)*T);
-//end
-//end
-//PSI0=PSI1;
-//PSI1=PSI;
-//APSI1=PSI1;
-//CHI0=CHI1;
-//CHI1=CHI;
-//XI1=APSI1-CHI1*sqrt(-1);
-//n=n+1;
-//RN=n;
-//for j=1:nang
-//PI1(j)=((2*RN-1)/(RN-1))*AMU(j)*PI(j)-RN*PI0(j)/(RN-1);
-//PI0(j)=PI(j);
-//end
-//end
-//Qscat=(2./x^2)*Qscat;
-//Qext=(4./x^2)*real(S1(1));
-//%Note: Qback is not Qbb, but the radar back scattering.
-//Qback=(4./x^2)*abs(S1(2*nang-1))^2;
+
+
+//// Function Definitions
 //
 //
-//
+int bhmie(double x,double refrel,int nang){
+
+    // Variable Definitions
+    complex<double> y; double dx; double nstop; double ymod; int nmx; double dang; double theta;
+    int nn;
+    double RN; double DN; double FN;
+    complex<double> AN; complex<double> BN;
+    double PSI; double PSI0; double PSI1;
+    double CHI; double CHI0; double CHI1;
+    double APSI; double APSI0; double APSI1;
+    complex <double> XI; complex <double> XI0; complex <double> XI1;
+    double Qscat; double Qext; double Qback;
+    double P; double T;
+
+
+    // Array Definitions
+    double PI[nang+1]; double PI0[nang+1]; double PI1[nang+1];
+    complex<double> S1[2*nang]; complex<double> S2[2*nang];
+
+    double AMU[nang+1]; double TAU[nang+1];
+
+
+    dx = x;
+    y = x * refrel;
+
+    nstop = ceil(x + 4 * pow(x,0.3333) +2);
+    ymod = abs(y);
+    nmx = max(nstop,ceil(ymod))+15;
+    complex<double> D[nmx];
+    dang = pi/2/(nang-1);
+
+
+    for (int i = 1; i<=nang; i++){
+        theta = (double)(i-1) * dang;
+        AMU[i] =  cos(theta);
+    }
+
+    //Logarithmic derivative D(j) calculated by downward recurence
+    D[nmx]= complex <double>(0,0);
+    nn = nmx-1;
+
+    for (int n = 1; n<=nn; n++){
+        RN = nmx-n+1;
+        D[nmx-n]=(RN/y)-(1.0/(D[nmx-n+1]+RN/y));
+    }
+
+    for (int j = 1; j <= nang; j++){
+        PI0[j] = 0.0;
+        PI1[j] = 1.0;
+    }
+
+    nn = 2*nang-1;
+
+    for (int j = 1; j <= nang; j++){
+        S1[j] = 0.0;
+        S2[j] = 1.0;
+    }
+
+    //Riccati-Bessel functins with real argument x calculated by upward recurence
+    PSI0=cos(dx);
+    PSI1=sin(dx);
+    CHI0=-sin(x);
+    CHI1=cos(x);
+    APSI0=PSI0;
+    APSI1=PSI1;
+    XI0=APSI0-CHI0*imaginary;
+    XI1=APSI1-CHI1*imaginary;
+    Qscat=0.0;
+
+    int n=1;
+    while(n-1-nstop<0){
+        DN=n;
+        RN=n;
+        FN=(2*RN+1)/(RN*(RN+1));
+        PSI=(2*DN-1)*PSI1/dx-PSI0;
+        APSI=PSI;
+        CHI=(2*RN-1)*CHI1/x-CHI0;
+        XI=APSI-CHI*imaginary;
+        AN=((D[n]/refrel+RN/x)*APSI-APSI1)/((D[n]/refrel+RN/x)*XI-XI1);
+        BN=((refrel*D[n]+RN/x)*APSI-APSI1)/((D[n]*refrel+RN/x)*XI-XI1);
+        Qscat=Qscat+(2.0*RN+1.0)*(abs(AN)*abs(AN)+abs(BN)*abs(BN));
+
+
+        for (int j=1; j<=nang; j++){
+            int jj=2*nang-j;
+            PI[j]=PI1[j];
+            TAU[j]=RN*AMU[j]*PI[j]-(RN+1)*PI0[j];
+            P=pow(-1.0,(n-1));
+            S1[j]=S1[j]+FN*(AN*PI[j]+BN*TAU[j]);
+            T=pow((-1),n);
+            S2[j]=S2[j]+FN*(AN*TAU[j]+BN*PI[j]);
+            if(j != jj){
+                S1[jj]=S1[jj]+FN*(AN*PI[j]*P+BN*TAU[j]*T);
+                S2[jj]=S2[jj]+FN*(BN*PI[j]*P+AN*TAU[j]*T);
+                if (jj>180){
+                    cout << S1[180] << endl;
+                }
+
+            }
+        }
+
+        PSI0=PSI1;
+        PSI1=PSI;
+        APSI1=PSI1;
+        CHI0=CHI1;
+        CHI1=CHI;
+        XI1=APSI1-CHI1*imaginary;
+        n=n+1;
+        RN=n;
+        for (int j=1; j<=nang; j++){
+            PI1[j]=((2*RN-1)/(RN-1))*AMU[j]*PI[j]-RN*PI0[j]/(RN-1);
+            PI0[j]=PI[j];
+        }
+    }
+    Qscat=(2.0/(x*x))*Qscat;
+    Qext=(4.0/(x*x))*real(S1[1]);
+    //Note: Qback is not Qbb, but the radar back scattering.
+    Qback=(4.0/(x*x))*(abs(S1[2*nang-1])*abs(S1[2*nang-1]));
+    //cout << "Qext = " << Qext << endl;
+    
+    cout << "Qback = " << Qback << endl;
+
+    return 0;
+}
+
+
+
